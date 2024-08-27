a9d56755eb0d0f3905d4628adc1228e64791754c
Dominggues <guilherme18domingues@gmail.com>
2024-08-27 17:24:30 -0300

    adicionando animação na navegação de itens da página

--- static/script.js
+++ static/script.js
@@ -22,3 +22,26 @@ navItem.forEach(item => {
     }
   })
 })
+
+/* Animar todos os itens que tiverem em meu atributo data-anime */
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+  console.log(windowTop)
+
+  item.forEach((element) => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add("animate");
+    } else {
+      element.classList.remove("animate");
+    }
+  });
+}
+
+animeScroll();
+
+window.addEventListener("scroll", ()=>{
+  animeScroll();
+})
